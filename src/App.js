7220af2db646cd8367f5dd7039bd39378f64feb8
Harry P <habs@sdf.org>
2021-03-04 02:01:21 -0500

    fix reading multiple layers to comply with spec

--- src/App.js
+++ src/App.js
@@ -3,43 +3,44 @@ import stringHash from 'string-hash';
 import {useState} from 'react';
 
 function App() {
-  const [u8s, setU8s] = useState([]);
+  const [bytes, setBytes] = useState([]);
   const [cols, setCols] = useState(20);
   const [layers, setLayers] = useState([]);
   const [selectedLayer, setSelectedLayer] = useState(0);
+  const [numLayers, setNumLayers] = useState(1);
 
-  const getLayers = (u8s) => {
-    let idx = 0;
-    let maxTiles = 0;
-    const newLayers = [];
-    while (idx < u8s.length - 12) {
-      const dv = new DataView(u8s.buffer.slice(idx, idx + 12));
-      const layerSize = dv.getUint32(0, false);
-      const layerId = dv.getUint32(4, false);
-      const layerZ = dv.getUint32(8, false);
-      const layerEnd = idx + layerSize + 4;
-      idx += 12;
-      console.groupCollapsed(`layer ${layerId} (sz ${layerSize}, z ${layerZ}):`);
-      const tiles = [];
-      let totalTiles = 0;
-      while (idx < layerEnd) {
-        const dv = new DataView(u8s.buffer.slice(idx, idx + 8));
-        const autotileIdx = dv.getInt16(0, false);
-        const tilesetId = dv.getInt16(2, false);
-        const tileId = dv.getInt16(4, false);
-        const numTiles = dv.getInt16(6, false);
-        totalTiles += numTiles;
-        console.log(autotileIdx, tilesetId, tileId, numTiles);
-        const color = '#' + stringHash(`${autotileIdx},${tilesetId},${tileId}`).toString(16).slice(0, 4);
-        tiles.push(...Array(numTiles).fill({ autotileIdx, tilesetId, tileId, color }))
+  const getLayers = (u8s = bytes) => {
+    try {
+      let idx = 0;
+      const dv = new DataView(u8s.buffer);
+      const newLayers = Array(numLayers).fill().map(_ => {
+        const size = dv.getUint32(idx, false);
+        idx += 4;
+        return { size };
+      })
+      newLayers.forEach(layer => {
+        //const groups = (layer.size - 8) / 8;
+        const layerEnd = idx + layer.size;
+        layer.layerId = dv.getUint32(idx, false);
+        layer.layerZ = dv.getUint32(idx + 4, false);
         idx += 8;
-      }
-      newLayers.push({layerId, layerZ, totalTiles, tiles});
-      console.log("i", idx, "/", u8s.length);
-      console.groupEnd();
-      idx = layerEnd; //?
+        console.groupCollapsed(`layer ${layer.layerId} (sz ${layer.size}, z ${layer.layerZ}):`);
+        layer.tiles = [];
+        for (; idx < layerEnd; idx += 8) {
+          const autotileIdx = dv.getInt16(idx, false);
+          const tilesetId = dv.getInt16(idx + 2, false);
+          const tileId = dv.getInt16(idx + 4, false);
+          const numTiles = dv.getInt16(idx + 6, false);
+          console.log({ autotileIdx, tilesetId, tileId, numTiles });
+          const color = '#' + stringHash(`${autotileIdx},${tilesetId},${tileId}`).toString(16).slice(0, 4);
+          layer.tiles.push(...Array(numTiles).fill({ autotileIdx, tilesetId, tileId, color }))
+        }
+        console.groupEnd();
+      });
+      setLayers(newLayers);
+    } catch (err) {
+      alert(`${err} (try a different number of layers?)`);
     }
-    setLayers(newLayers);
   }
 
   const formatCell = (cell, ci) => {
@@ -48,7 +49,7 @@ function App() {
   }
 
   const layer = layers[selectedLayer] || {};
-  const rows = layer.totalTiles ? Math.ceil(layer.totalTiles / cols) : 0;
+  const rows = layer.tiles ? Math.ceil(layer.tiles.length / cols) : 0;
 
   return (
     <div className="App">
@@ -57,12 +58,15 @@ function App() {
           const reader = new FileReader();
           const file = evt.target.files[0];
           reader.onload = () => {
-            const bytes = new Uint8Array(reader.result);
-            setU8s(bytes);
-            getLayers(bytes);
+            const data = new Uint8Array(reader.result);
+            setBytes(data);
+            getLayers(data);
           }
           reader.readAsArrayBuffer(file);
         }} />
+        <div><label htmlFor="numLayers">Number of layers: </label></div>
+        <input type="number" name="numLayers" value={numLayers} onChange={(e) => setNumLayers(Number(e.target.value))} />
+        <button onClick={() => getLayers()}>Re-calculate layers</button>
         <div><label htmlFor="cols">Columns: </label></div>
         <input type="number" name="cols" value={cols} onChange={(e) => setCols(Number(e.target.value))} />
         <div><label htmlFor="layer">Selected Layer: </label></div>
