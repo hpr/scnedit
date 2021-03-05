9d36cc4dde5ddf5a47d51bd89ff0c10117374e4e
Harry P <habs@sdf.org>
2021-03-04 19:01:48 -0500

    add show cell text option and paint selector

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
   const [paintMode, setPaintMode] = useState(false);
   const [bucket, setBucket] = useState("0,-1,-1");
   const [filename, setFilename] = useState("");
+  const [showCellText, setShowCellText] = useState(false);
 
   const getColor = (autotileIdx, tilesetId, tileId) => '#' + stringHash(`${autotileIdx},${tilesetId},${tileId}`).toString(16).slice(0, 4);
 
@@ -81,9 +82,11 @@ function App() {
     saveAs(blob, filename);
   }
 
+
+  const cellStyle = { height: showCellText ? 0 : '2em', width: showCellText ? 'auto' : '2em' };
   const formatCell = (cell, ci, idx) => {
-    if (!cell) return <td key={ci}></td>
-    return <td key={ci} name={idx} style={{ background: cell.color }}>{cell.autotileIdx},{cell.tilesetId},{cell.tileId}</td>
+    if (!cell) return <td style={cellStyle} key={ci}></td>
+    return <td key={ci} name={idx} style={{ ...cellStyle, background: cell.color }}>{ showCellText && `${cell.autotileIdx},${cell.tilesetId},${cell.tileId}` }</td>
   }
 
   const layer = layers[selectedLayer] || {};
@@ -91,62 +94,68 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <div>
-          <input type="file" accept=".scn" onChange={(evt) => {
-            const reader = new FileReader();
-            const file = evt.target.files[0];
-            reader.onload = () => {
-              const data = new Uint8Array(reader.result);
-              setBytes(data);
-              setFilename(file.name);
-              getLayers(data);
-            }
-            reader.readAsArrayBuffer(file);
-          }} />
-          <button onClick={() => exportScn()}>Export changes</button>
-        </div>
-        <div>
-          <label htmlFor="numLayers">Number of layers: </label>
-          <input type="number" name="numLayers" value={numLayers} onChange={(e) => setNumLayers(Number(e.target.value))} />
-          <button onClick={() => getLayers()}>Re-calculate layers</button>
-        </div>
-        <div>
-          <label htmlFor="cols">Columns: </label>
-          <input type="number" name="cols" value={proposedCols} onChange={(e) => setProposedCols(Number(e.target.value))} />
-          <button onClick={() => setCols(Number(proposedCols))}>Set columns</button>
-        </div>
-        <div>
-          <label htmlFor="layer">Selected layer: </label>
-          <select name="layer" value={selectedLayer} onChange={(e) => setSelectedLayer(Number(e.target.value))}>
-            {layers.map((layer, i) => <option key={i} value={i}>{layer.layerId} (z = {layer.layerZ})</option>)}
-          </select>
-        </div>
-        <div>
-          <label htmlFor="paintMode">Paint mode: </label>
-          <input type="checkbox" name="paintMode" value={paintMode} onChange={() => setPaintMode(!paintMode)}/>
-        </div>
-        <div><label htmlFor="bucket">Paint with: </label><input type="text" name="bucket" value={bucket} onChange={(e) => setBucket(e.target.value)} /></div>
-        <table onClick={(evt) => {
-          if (!paintMode) return;
-          const i = Number(evt.target.getAttribute('name'));
-          const [autotileIdx, tilesetId, tileId] = bucket.split(',');
-          const color = getColor(autotileIdx, tilesetId, tileId);
-          const newLayers = [...layers];
-          const newSelectedLayer = {...layers[selectedLayer]};
-          const newTiles = [...newSelectedLayer.tiles];
-          newLayers[selectedLayer] = newSelectedLayer;
-          newSelectedLayer.tiles = newTiles;
-          newTiles[i] = { autotileIdx, tilesetId, tileId, color };
-          setLayers(newLayers);
-        }}>
-          <tbody>
-            {cols ? Array(rows).fill().map((_, ri) => <tr key={ri}>
-              {Array(cols).fill().map((_, ci) => formatCell(layer.tiles[ri * cols + ci], ci, ri * cols + ci))}
-            </tr>) : null}
-          </tbody>
-        </table>
-      </header>
+        <header className="App-header">
+            <div>
+                <input type="file" accept=".scn" onChange={(evt) => {
+                  const reader = new FileReader();
+                  const file = evt.target.files[0];
+                  reader.onload = () => {
+                    const data = new Uint8Array(reader.result);
+                    setBytes(data);
+                    setFilename(file.name);
+                    getLayers(data);
+                  }
+                  reader.readAsArrayBuffer(file);
+                }} />
+                <button onClick={() => exportScn()}>Export changes</button>
+            </div>
+            <div>
+                <label htmlFor="numLayers">Number of layers: </label>
+                <input type="number" name="numLayers" value={numLayers} onChange={(e) => setNumLayers(Number(e.target.value))} />
+                <button onClick={() => getLayers()}>Re-calculate layers</button>
+            </div>
+            <div>
+                <label htmlFor="cols">Columns: </label>
+                <input type="number" name="cols" value={proposedCols} onChange={(e) => setProposedCols(Number(e.target.value))} />
+                <button onClick={() => setCols(Number(proposedCols))}>Set columns</button>
+            </div>
+            <div>
+                <label htmlFor="layer">Selected layer: </label>
+                <select name="layer" value={selectedLayer} onChange={(e) => setSelectedLayer(Number(e.target.value))}>
+                    {layers.map((layer, i) => <option key={i} value={i}>{layer.layerId} (z = {layer.layerZ})</option>)}
+                </select>
+            </div>
+            <div>
+                <label htmlFor="showCellText">Show cell text? </label>
+                <input type="checkbox" name="showCellText" value={showCellText} onChange={() => setShowCellText(!showCellText)}/>
+                <label htmlFor="paintMode">Paint mode: </label>
+                <input type="checkbox" name="paintMode" value={paintMode} onChange={() => setPaintMode(!paintMode)}/>
+            </div>
+            <div><label htmlFor="bucket">Paint with: </label><input type="text" name="bucket" value={bucket} onChange={(e) => setBucket(e.target.value)} /></div>
+            <table onClick={(evt) => {
+              const i = Number(evt.target.getAttribute('name'));
+              if (!paintMode) {
+                const tile = layers[selectedLayer].tiles[i];
+                setBucket(`${tile.autotileIdx},${tile.tilesetId},${tile.tileId}`);
+                return;
+              }
+              const [autotileIdx, tilesetId, tileId] = bucket.split(',');
+              const color = getColor(autotileIdx, tilesetId, tileId);
+              const newLayers = [...layers];
+              const newSelectedLayer = {...layers[selectedLayer]};
+              const newTiles = [...newSelectedLayer.tiles];
+              newLayers[selectedLayer] = newSelectedLayer;
+              newSelectedLayer.tiles = newTiles;
+              newTiles[i] = { autotileIdx, tilesetId, tileId, color };
+              setLayers(newLayers);
+            }}>
+                <tbody>
+                    {cols ? Array(rows).fill().map((_, ri) => <tr key={ri}>
+                        {Array(cols).fill().map((_, ci) => formatCell(layer.tiles[ri * cols + ci], ci, ri * cols + ci))}
+                    </tr>) : null}
+                </tbody>
+            </table>
+        </header>
     </div>
   );
 }
